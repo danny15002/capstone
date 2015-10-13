c23395d645768e0dbdfa78599b917827cfa9cbe3
Daniel Bellini <danny15002@gmail.com>
2015-10-13 12:24:07 -0700

    retrieve user started

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013020725) do
+ActiveRecord::Schema.define(version: 20151013182626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "from_id",                    null: false
+    t.integer  "to_id",                      null: false
+    t.text     "body",                       null: false
+    t.boolean  "public",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "messages", ["from_id"], name: "index_messages_on_from_id", using: :btree
+  add_index "messages", ["to_id"], name: "index_messages_on_to_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
