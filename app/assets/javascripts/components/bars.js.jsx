72596c77f3e7157fffeb38825410c0a67210f6f7
Daniel Bellini <danny15002@gmail.com>
2015-10-14 09:12:01 -0700

    can navigate home

--- app/assets/javascripts/components/bars.js.jsx
+++ app/assets/javascripts/components/bars.js.jsx
@@ -19,7 +19,6 @@ var LeftBar = React.createClass ({
         break;
       default:
         break;
-
     }
   },
 
@@ -36,11 +35,36 @@ var LeftBar = React.createClass ({
 })
 
 var RightBar = React.createClass ({
+  mixins: [ReactRouter.History],
+
+  handleClick: function (event) {
+    console.log($(event.target).context.innerText);
+    var selected = $(event.target).context.innerText;
+    switch(selected) {
+      case "Home":
+        this.history.pushState(null, "/")
+        break;
+      case "Events":
+        this.history.pushState(null, "/" + selected)
+        break;
+      case "Pictures":
+        this.history.pushState(null, "/" + selected)
+        break;
+      case "Location":
+        this.history.pushState(null, "/" + selected)
+        break;
+      default:
+        break;
+    }
+  },
+
   render: function () {
     return (
-      <div className={"bar right-bar"}>
-
-
+      <div onClick={this.handleClick} className={"bar right-bar"}>
+        <div selected={false} className={"nav nav-messages"}><div>Home</div></div>
+        <div selected={false} className={"nav nav-events"}><div>Events</div></div>
+        <div selected={false} className={"nav nav-pictures"}><div>Pictures</div></div>
+        <div selected={false} className={"nav nav-location"}><div>Location</div></div>
       </div>
     )
   }
