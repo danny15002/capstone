a295a6ed261e6d60c409949355b9d2a05db5fef1
Daniel Bellini <danny15002@gmail.com>
2015-10-19 17:27:57 -0700

    comment boxes on public posts show up

--- app/assets/javascripts/components/message_index.js.jsx
+++ app/assets/javascripts/components/message_index.js.jsx
@@ -1,10 +1,14 @@
 var MessageIndex = React.createClass( {
   getInitialState: function () {
-    return {messages: []}
+    return {messages: [], formStyle: "none" }
   },
   componentDidMount: function () {
+    var id;
     MessageStore.addChangeListener(FriendzConstants.MESSAGES_RECEIVED, this.getMessages);
-    ApiUtil.fetchMessages(true);
+    if (this.props.userId !== undefined) {
+      id = parseInt(this.props.userId);
+    }
+    ApiUtil.fetchMessages(true, id);
   },
   componentWillUnmount: function () {
     MessageStore.removeChangeListener(FriendzConstants.MESSAGES_RECEIVED, this.getMessages);
@@ -12,6 +16,7 @@ var MessageIndex = React.createClass( {
   getMessages: function () {
     this.setState({messages: MessageStore.getMessages()})
   },
+
   render: function () {
     return (
       <div className={"message-index"}>
@@ -20,18 +25,39 @@ var MessageIndex = React.createClass( {
         </div>
         <ul>
           {this.state.messages.map( function (message) {
+            var comments = <ul></ul>
+            if (message.comments !== undefined) {
+              comments = (
+                <ul>
+                  {message.comments.map(function (comment) {
+                    return (
+                      <li key={comment.id}>
+                        <Comment
+                          value={comment.body}
+                          className={"post-comment"}/>
+                      </li>
+                    )
+                  })}
+                </ul>
+              )
+            }
             return (
               <li key={message.id} className={"message"}>
                 <div>
-                  <div className={"msg-sender"}>{message.sender_name}</div>
+                  <div className={"msg-sender"}>
+                    {message.sender_name + " -> " + message.recipient_name}
+                  </div>
                   <div className={"msg-created"}>{message.created_at}</div>
                 </div>
                 <br></br>
                 <br></br>
                 <p className={"msg-body"}>{message.body}</p>
+                <div>likes</div>
+                <CommentForm/>
+                {comments}
               </li>
             )
-          })}
+          }.bind(this))}
         </ul>
       </div>
     )
--- app/views/api/messages/index.json.jbuilder
+++ app/views/api/messages/index.json.jbuilder
@@ -3,6 +3,7 @@ json.array! @messages do |message|
   json.body message.body
   json.recipient_name message.user_to.username
   json.sender_name message.user_from.username
+  json.comments message.comments
 
   json.created_at message.format_message_time
 end
