cdf670df6d148a24ce87beaa76718a494d97b378
Daniel Bellini <danny15002@gmail.com>
2015-10-19 20:50:07 -0700

    nested comments handled recursively

--- app/assets/javascripts/components/comment.js.jsx
+++ app/assets/javascripts/components/comment.js.jsx
@@ -1,9 +1,21 @@
 var Comment = React.createClass({
-
+  subComments: function (comments) {
+    return <SubCommentList comments={comments} />
+  },
   render: function () {
+    var message = this.props.message;
+    var subComments=<div></div>
+
+    if (message.comments) {
+      if (message.comments.length > 0) {
+        subComments = this.subComments(message.comments);
+      }
+    }
+    
     return (
       <div className={this.props.className + " comment"}>
-        {this.props.value}
+        {message.body}
+        {subComments}
       </div>
     )
   }
