d55acdc2da1795f6b7351a125deb4df3c2f75fd7
Daniel Bellini <danny15002@gmail.com>
2015-10-19 22:39:51 -0700

    refactored comments

--- app/assets/javascripts/components/comment_form.js.jsx
+++ app/assets/javascripts/components/comment_form.js.jsx
@@ -1,23 +1,27 @@
 var CommentForm = React.createClass({
   getInitialState: function () {
-    return {formStyle: "none" }
+    return {formStyle: "none", subText: "add comment" }
   },
   componentWillReceiveProps: function (nextProps) {
   },
   showForm: function () {
     if (this.state.formStyle === "none") {
-      this.setState({formStyle: ""});
+      this.setState({formStyle: "",subText: "hide comment box"});
     } else {
-      this.setState({formStyle: "none"});
+      this.setState({formStyle: "none", subText: "add comment"});
     }
+  },
+  submitForm: function () {
+
   },
   render: function () {
     return(
       <div>
-        <div onClick={this.showForm}>add comment</div>
-        <textarea style={{display: this.state.formStyle}} placeholder={"Leave a comment."}>
-
-        </textarea>
+        <div onClick={this.showForm}>{this.state.subText}</div>
+        <div style={{display: this.state.formStyle}}>
+          <textarea placeholder={"Leave a comment."}/>
+          <button className={"comment-button"} onClick={this.submitForm}>Submit Comment</button>
+        </div>
       </div>
     )
   }
